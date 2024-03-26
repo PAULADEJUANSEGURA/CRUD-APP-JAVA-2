df19bdc64e4aa4bba01c309d7c05e55e535c899d
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2024-03-26 17:53:27 +0100

    primer commit springboot

---
+++ src/main/java/com/bestfriends/demo/BestFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.bestfriends.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestfriends/demo/BestFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestfriends.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
